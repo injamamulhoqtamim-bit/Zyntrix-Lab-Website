597c75740614c38c38a2474b724597df8528ecca
Md Injamamul Hoq Tamim <injamamulhoqtamim@gmail.com>
2026-03-18 18:31:07 +0600

    message

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ document.getElementById("contactForm").addEventListener("submit", function(e) {
 
   this.reset();
 });
+
+// ===== HERO VIDEO PLAYLIST =====
+
+const heroVideo = document.getElementById("heroVideo");
+
+// 🔥 Video list (assets folder এ রাখবে)
+const videoList = [
+  "./assets/hero background - Trim.mp4",
+  "./assets/hero3 - Trim.mp4",
+  "./assets/hero4 - Trim.mp4"
+];
+
+let currentVideo = 0;
+
+// First video load
+heroVideo.src = videoList[currentVideo];
+
+// When video ends → play next
+heroVideo.addEventListener("ended", () => {
+  currentVideo = (currentVideo + 1) % videoList.length;
+  heroVideo.src = videoList[currentVideo];
+  heroVideo.play();
+});
